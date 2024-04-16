92a9b8510997c21015011111505b0d320a92b179
rynsf <rynsf@users.noreply.github.com>
2024-04-16 16:55:01 +0530

    Add handlers for like and no-like

--- like-my-site/cmd/web/main.go
+++ like-my-site/cmd/web/main.go
@@ -8,7 +8,12 @@ import (
 )
 
 func handlerLikeMe(w http.ResponseWriter, r *http.Request) {
-    page, err := template.ParseFiles("ui/html/home.tmpl.html", "ui/html/like-button.tmpl.html", "ui/html/liked-button.tmpl.html")
+    page, err := template.ParseFiles(
+        "ui/html/home.tmpl.html",
+        "ui/html/like.tmpl.html",
+        "ui/html/like-button.tmpl.html",
+        "ui/html/liked-button.tmpl.html",
+        )
     if err != nil {
         log.Fatal(err.Error())
     }
@@ -18,9 +23,39 @@ func handlerLikeMe(w http.ResponseWriter, r *http.Request) {
     }
 }
 
+func handlerLike(w http.ResponseWriter, r *http.Request) {
+    t, err := template.ParseFiles(
+        "ui/html/no-like.tmpl.html",
+        "ui/html/liked-button.tmpl.html",
+        )
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+    err = t.ExecuteTemplate(w, "no-like", nil)
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+}
+
+func handlerNoLike(w http.ResponseWriter, r *http.Request) {
+    t, err := template.ParseFiles(
+        "ui/html/like.tmpl.html",
+        "ui/html/like-button.tmpl.html",
+        )
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+    err = t.ExecuteTemplate(w, "like", nil)
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+}
+
 func main() {
     mux := http.NewServeMux()
     mux.HandleFunc("/", handlerLikeMe)
+    mux.HandleFunc("/like", handlerLike)
+    mux.HandleFunc("/no-like", handlerNoLike)
 
     log.Println("Listening on :8080")
     err := http.ListenAndServe(":8080", mux)
